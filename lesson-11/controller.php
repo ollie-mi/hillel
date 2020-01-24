e2299cb11d28c52e8c4098b062e4e193f5a70ae0
ollie <o.mialkivska@gmail.com>
2020-01-24 20:49:39 +0200

    lesson-11 hw

--- lesson-11/controller.php
+++ lesson-11/controller.php
@@ -19,21 +19,15 @@ foreach ($requiredFields as $field) {
     if (isset($_POST[$field]) && strlen($_POST[$field])) {
         $rawData[$field] = trim($_POST[$field]);
     } else {
+        // Если хотя бы одно из полей пустое, выводим ошибку
         $_SESSION['errors'] = [
             $field => [
                 'message' => 'The field must be filled in!'
             ]
         ];
+        header('Location: /lesson-11/form.phtml');
+        exit();
     }
-}
-
-/*
- * Если хотя бы одно из полей пустое, выводим ошибку
- */
-if (isset($_SESSION['errors'])) {
-//    header('Location: /lesson-11/form.phtml');
-    header('Location: form.php');
-    exit();
 }
 
 /*
@@ -42,7 +36,7 @@ if (isset($_SESSION['errors'])) {
 
 $data = [
     'email' => htmlspecialchars($rawData['email']),
-    'age' => (int) $rawData['age'],
+    'age' => (int)$rawData['age'],
     'password' => password_hash($rawData['password'], PASSWORD_DEFAULT)
 ];
 
@@ -51,40 +45,42 @@ $data = [
  */
 
 if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
-    $_SESSION['errors'] = [
-        'email' => [
-            'message' => 'Email address is not valid!'
-        ]
+    $_SESSION['errors']['email'] = [
+        'message' => 'Email address is not valid!'
     ];
 }
 
 $requiredAge = 14;
 if ($data['age'] < $requiredAge) {
-    $_SESSION['errors'] = [
-        'age' => [
-            'message' => 'Sorry! You are to young!'
-        ]
+    $_SESSION['errors']['age'] = [
+        'message' => 'Sorry! You are to young!'
     ];
 }
 
 if ($rawData['password'] !== $rawData['password_confirm']) {
-    $_SESSION['errors'] = [
-        'password_confirm' => [
-            'message' => 'Incorrect password confirmation!'
-        ]
+    $_SESSION['errors']['password_confirm'] = [
+        'message' => 'Incorrect password confirmation!'
     ];
 }
 
 if (isset($_SESSION['errors'])) {
-//    header('Location: /lesson-11/form.phtml');
-    header('Location: form.php');
+    header('Location: /lesson-11/form.phtml');
     exit();
 } else {
-    $filename = __DIR__ . '/database/users.json';
+    $dir = __DIR__ . '/database';
+    // Если нет директории, создаём её
+    if (!mkdir($dir, 0777, true) && !is_dir($dir)) {
+        throw new \RuntimeException(sprintf('Directory "%s" was not created', $dir));
+    }
+
+    $filepath = $dir . '/users.json';
     $addUsers = [];
-    if (file_exists($filename)) {
-        $content = file_get_contents($filename);
-        $users = json_decode($content, true);
+    if (file_exists($filepath)) {
+        /*
+         * Если файл уже существует, проверяем нет ли там юзера с таким же имейлом
+         */
+        $content = file_get_contents($filepath);
+        $users = json_decode($content, true, 512, JSON_THROW_ON_ERROR);
         foreach ($users as $user) {
             if ($data['email'] === $user['email']) {
                 $_SESSION['errors'] = [
@@ -92,8 +88,7 @@ if (isset($_SESSION['errors'])) {
                         'message' => 'This email already exists! Please, choose another one'
                     ]
                 ];
-//                header('Location: /lesson-11/form.phtml');
-                header('Location: form.php');
+                header('Location: /lesson-11/form.phtml');
                 exit();
             } else {
                 $addUsers[] = $user;
@@ -101,55 +96,28 @@ if (isset($_SESSION['errors'])) {
         }
         $addUsers[] = $data;
         file_put_contents(
-            $filename,
-            json_encode($addUsers, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT)
+            $filepath,
+            json_encode($addUsers, JSON_THROW_ON_ERROR | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT, 512)
         );
-        //        header('Location: /lesson-11/form.phtml');
-        header('Location: form.php');
-        exit();
     } else {
         /*
         * Записываем данные в файл
         */
         $addUsers[] = $data;
         file_put_contents(
-            $filename,
-            json_encode($addUsers, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT)
+            $filepath,
+            json_encode($addUsers, JSON_THROW_ON_ERROR | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT, 512)
         );
+    }
 
+    /*
+     * Если не было ошибок, юзер зарегистрирован
+     */
+    if (!isset($_SESSION['errors'])) {
         $_SESSION['success'] = [
             'message' => 'Congratulations! You are registered now!'
         ];
-
-        /*
-        * Перенаправляем обратно
-        */
-//        header('Location: /lesson-11/form.phtml');
-        header('Location: form.php');
+        header('Location: /lesson-11/form.phtml');
         exit();
     }
 }
-
-
-//if (isset($_POST['year'], $_POST['month'], $_POST['day'])) {
-//
-//    $year = (int)$_POST['year'];
-//    $month = (int)$_POST['month'];
-//    $day = (int)$_POST['day'];
-//
-//    $requiredTime = strtotime('-18 years');
-//    $userTime = strtotime("{$year}/{$month}/{$day}");
-//
-//    if ($userTime < $requiredTime) {
-//        // Проходи
-//    } else {
-//        $_SESSION['errors'] = [
-//            'age' => [
-//                'value' => $year,
-//                'message' => 'Доступ запрещен!'
-//            ]
-//        ];
-//
-//        header('Location: /lesson-11/validation/form.php');
-//    }
-//}
